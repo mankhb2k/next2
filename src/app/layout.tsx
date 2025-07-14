ac69f863de26c891e426da6c5e7875da01d66e10
mankhb2k <mankhb2k@gmail.com>
2025-07-14 15:36:20 +0700

    lam sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,27 @@
 import "./globals.css";
-import { ThemeProvider } from "@/components/providers/theme-provider"
+import { SidebarProvider, SidebarContent, SidebarFooter,  SidebarGroup, SidebarHeader, SidebarTrigger } from "@/components/ui/sidebar";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { AppSidebar } from "@/components/app-sidebar";
 
+export const metadata = {
+  title: "Sidebar Layout Example",
+  description: "Sidebar + Content layout with shadcn/ui and next-themes",
+};
 
-export default function RootLayout({ children }: {children: React.ReactNode;}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <html lang="vi" suppressHydrationWarning>
-        <head />
-        <body>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
-  )
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <SidebarProvider>
+            <AppSidebar />
+            <main>
+             <SidebarTrigger />
+             {children}
+           </main>
+          </SidebarProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
